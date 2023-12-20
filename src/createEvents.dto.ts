b1b13d75ad78582cd9c094e57930d86165b69504
Zohaib Muhammad Awan <59654046+MuhammadZohaibAwan@users.noreply.github.com>
2023-12-20 12:55:27 +0500

    validation-complete

--- src/createEvents.dto.ts
+++ src/createEvents.dto.ts
@@ -1,9 +1,13 @@
 import { IsString,IsDateString, Length, isString } from "class-validator";
 
-export class CreateEventsDto {
+// global validation = main.ts enabled + add validation inside dto
+// validation can be grouped or can be global
+// (@Body(new ValidationPipe({groups:['create']}))) + disable global validation while using local validation
+//@UsePipe = on controller specific router e;g @Get or on class level if you use it class level
 
+export class CreateEventsDto {
     @IsString()
-    @Length(5,255,{message: 'The name length is wrong'})
+    @Length(5,255,{message: 'The name length is wrong'})//length of name to 5-255 validation pipe
     name: string;
     @Length(5,255)
     description: string;
