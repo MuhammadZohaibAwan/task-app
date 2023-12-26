8f41c6b88327a29a73801677c996f2189cfb2924
Zohaib Muhammad Awan <prinxzohaib@gmail.com>
2023-12-26 12:42:11 -0800

    updates-26-dec

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,8 +18,8 @@ import { EventsEntity } from './events/events.entity';
     // port: 5432,
     host: process.env.DB_HOST,
     port:Number( process.env.DB_PORT),
-    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    username: process.env.DB_USER,
     database: process.env.DB_NAME,
     entities: [EventsEntity],
     synchronize: true
